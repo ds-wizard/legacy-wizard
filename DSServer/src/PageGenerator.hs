5534a1f3a65b685f28237ada8cad16adccb0280c
Robert Pergl <robert@pergl.org>
2017-01-12 21:22:47 +0100

    - updated FormEngine - updated KM

--- DSServer/src/PageGenerator.hs
+++ DSServer/src/PageGenerator.hs
@@ -29,7 +29,7 @@ renderBanner = H.div ! A.id "banner" $ do
     H.img ! A.src (textValue $ staticURL <> "img/logo.png") ! A.id "logo" ! A.alt "Elixir logo"
   H.h1 ! A.class_ "title" $ do
     "Data Stewardship Wizard"
-    H.span ! A.class_ "version" $ " v0.1.1, "
+    H.span ! A.class_ "version" $ " v0.1.2, "
     H.span ! A.class_ "version" $ " KM: Jan 5, 2017"
 
 renderFooter :: Html
