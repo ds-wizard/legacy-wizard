102b01918642731c9ef5b9ae220145b270bfe133
Robert Pergl <robert@pergl.org>
2017-03-14 11:06:11 +0100

    edit.sh script improvement

--- DSServer/edit.sh
+++ DSServer/edit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gvim '+map <leader>r :!bin/reload.sh<cr>' '+:Lexplore<cr>' '+:vertical resize 30<cr>'
+gvim '+map <leader>r :!bin/reload.sh<cr>' '+:tabnew<cr>' '+:Lexplore<cr>' '+:vertical resize 30<cr>'
