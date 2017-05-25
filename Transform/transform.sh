ea3d25b59345070d45ae2e53a608d4dfed55698a
Robert Pergl <robert@pergl.org>
2017-05-25 20:27:37 +0200

    transform.sh : Overloaded Strings

--- Transform/transform.sh
+++ Transform/transform.sh
@@ -2,5 +2,6 @@
 NEW_DSKM=$(dist/build/Transform/Transform $1)
 
 rm -f Questionnaire.hs
-sed '/^formItems = .*$/d' src/Questionnaire.template.hs > dist/Questionnaire.hs
+echo "{-# LANGUAGE OverloadedStrings #-}" > dist/Questionnaire.hs
+sed '/^formItems = .*$/d' src/Questionnaire.template.hs >> dist/Questionnaire.hs
 echo "formItems = prepareForm $NEW_DSKM" >> dist/Questionnaire.hs
