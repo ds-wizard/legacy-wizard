933eda429414b4f78728ad9ef9fc12ae734d00ca
Robert Pergl <robert@pergl.org>
2017-05-26 21:24:38 +0200

    whitespaces

--- DSServer/app/Persistence/Utils.hs
+++ DSServer/app/Persistence/Utils.hs
@@ -3,10 +3,10 @@
 module Persistence.Utils where
 
 import qualified Data.Text as T
-import System.Random (randomRs, getStdGen)
+import System.Random (randomRs, newStdGen)
 
 genKey :: IO T.Text
 genKey = do
-  stdGen <- getStdGen
+  stdGen <- newStdGen
   return $ T.pack $ take 30 $ randomRs ('a','z') stdGen
 
