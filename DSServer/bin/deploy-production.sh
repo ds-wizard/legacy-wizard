ace296b7872959976ebb438b97d5976ae4c8ee92
Robert Pergl <robert@pergl.org>
2017-07-05 16:15:49 +0200

    script update

--- DSServer/bin/deploy-production.sh
+++ DSServer/bin/deploy-production.sh
@@ -6,4 +6,4 @@ stack install DSServer
 rsync -rvz --delete -e ssh bin static rob@ccmi.fit.cvut.cz:$D
 rsync -rvz --delete -e ssh /home/rob/.local/bin/DSServer rob@ccmi.fit.cvut.cz:$D/dmp.fairdata.solutions
 
-ssh -t rob@ccmi.fit.cvut.cz 'sudo chown -R www-data.www-data $D; sudo systemctl restart dmp.fairdata.solutions.service'
+ssh -t rob@ccmi.fit.cvut.cz 'sudo systemctl restart dmp.fairdata.solutions.service'
