4d82f92086daf37ca7addf0ad96e80b91d16b0e9
Robert Pergl <robert@pergl.org>
2017-08-31 11:35:22 +0200

    transform.sh removed obsolete line

--- Transform/transform.sh
+++ Transform/transform.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 NEW_DSKM=`stack exec Transform $1`
 
-rm -f Questionnaire.hs
 echo "{-# LANGUAGE OverloadedStrings #-}" > dist/Questionnaire.hs
 sed '/^formItems = .*$/d' src/Questionnaire.template.hs >> dist/Questionnaire.hs
 echo "formItems = prepareForm $NEW_DSKM" >> dist/Questionnaire.hs
